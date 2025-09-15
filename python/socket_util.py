ecb0090328794cca1389bb4b3a866b43ad2f1f69
b1az3ng <b1az3ng@github.com>
2025-09-15 19:46:07 +0800

    asd

--- python/socket_util.py
+++ python/socket_util.py
@@ -3,19 +3,31 @@ import json
 
 class TribesServer:
     def __init__(self, host="localhost", port=5000):
+        self.host = host
+        self.port = port
+        self.connect()
+
+    def connect(self):
         self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server.bind((host, port))
+        self.server.bind((self.host, self.port))
         self.server.listen(1)
-        print(f"[Python] Waiting for Java on {host}:{port}...")
+        print(f"[Python] Waiting for Java on {self.host}:{self.port}...")
         self.conn, addr = self.server.accept()
         print(f"[Python] Java connected from {addr}")
 
+    def reconnect (self):
+        self.connect()
+        self.run()
     def run(self):
         while True:
-            data = self.conn.recv(4096).decode()
+            try:
+                data = self.conn.recv(4096).decode()
+            except:
+                print("Disconnected: Reconnecting")
+                self.reconnect()
             if not data:
                 break
-
+            
             # Parse JSON from Java
             payload = json.loads(data)
             available = payload["available_actions"]
